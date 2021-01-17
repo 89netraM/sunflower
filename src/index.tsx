f5740b7d9c7b8e42a7528be7f06c578c9aff96df
Mårten Åsberg <marten.asberg@outlook.com>
2021-01-17 08:57:40 +0100

    Initialize React

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.scss";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<h1>Sunflower</h1>
+	</React.StrictMode>,
+	document.querySelector("app")
+);
