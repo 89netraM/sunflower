037329fb77aa9c107466070e3b5d8f35c1458082
Mårten Åsberg <marten.asberg@outlook.com>
2021-01-17 16:37:27 +0100

    Highlight

--- src/Sunflower.tsx
+++ src/Sunflower.tsx
@@ -1,15 +1,27 @@
 import React, { Component, createRef, ReactNode, RefObject } from "react";
-import { getAccentColor } from "./colorFunctions";
+import { getAccentColor, getComplimentColor } from "./colorFunctions";
 import { NumberInput } from "./NumberInput";
 
 interface SunflowerState {
 	numPoints: number,
 	turnFraction: number,
 	pow: number,
+	shouldHighlight: boolean,
+	highlight: number,
+	highlightOffset: number,
 }
 
 export class Sunflower extends Component<{}, SunflowerState> {
 	private static readonly dotRadius: number = 2.5;
+	private static readonly fibMap: { [n: number]: number } = {
+		89: 34,
+		55: 21,
+		34: 13,
+		21: 8,
+		13: 5,
+		8: 3,
+		5: 2
+	};
 
 	private readonly canvas: RefObject<HTMLCanvasElement> = createRef();
 	private readonly canvasReplacement: RefObject<HTMLDivElement> = createRef();
@@ -27,13 +39,22 @@ export class Sunflower extends Component<{}, SunflowerState> {
 		};
 	}
 
+	private get stepSize(): number {
+		return this.state.highlight in Sunflower.fibMap ?
+			Sunflower.fibMap[this.state.highlight] :
+			1;
+	}
+
 	public constructor(properties: {}) {
 		super(properties);
 
 		this.state = {
-			numPoints: 100,
+			numPoints: 1000,
 			turnFraction: 1.618,
 			pow: 0.5,
+			shouldHighlight: false,
+			highlight: 34,
+			highlightOffset: 0,
 		};
 	}
 
@@ -68,14 +89,21 @@ export class Sunflower extends Component<{}, SunflowerState> {
 			const x = dst * Math.cos(angle);
 			const y = dst * Math.sin(angle);
 
-			this.drawPoint(x, y);
+			this.drawPoint(
+				x,
+				y,
+				this.state.shouldHighlight &&
+					(i + this.state.highlightOffset * this.stepSize) % this.state.highlight === 0 ?
+					getComplimentColor() :
+					getAccentColor()
+			);
 		}
 	}
-	private drawPoint(x: number, y: number): void {
+	private drawPoint(x: number, y: number, color: string): void {
 		const canvasX = this.offset.x + (x + 1) * this.size / 2 - Sunflower.dotRadius / 2;
 		const canvasY = this.offset.y + (y + 1) * this.size / 2 - Sunflower.dotRadius / 2;
 
-		this.ctx.fillStyle = getAccentColor();
+		this.ctx.fillStyle = color;
 		this.ctx.beginPath();
 		this.ctx.ellipse(
 			canvasX,
@@ -121,6 +149,40 @@ export class Sunflower extends Component<{}, SunflowerState> {
 					>
 						Power:
 					</NumberInput>
+					<hr />
+					<p>
+						<label>
+							Highlight:
+							<input
+								type="checkbox"
+								onChange={e => this.setState({ shouldHighlight: e.target.checked })}
+							/>
+							<span className="toggle" />
+						</label>
+					</p>
+					<div hidden={!this.state.shouldHighlight}>
+						<NumberInput
+							value={this.state.highlight}
+							min={0}
+							step={1}
+							suggestedMax={89}
+							onChange={v => this.setState({
+								highlight: v,
+								highlightOffset: Math.min(this.state.highlightOffset, v)
+							})}
+						>
+							Offset:
+						</NumberInput>
+						<NumberInput
+							value={this.state.highlightOffset}
+							min={0}
+							step={1}
+							suggestedMax={this.state.highlight}
+							onChange={v => this.setState({ highlightOffset: v })}
+						>
+							Arm:
+						</NumberInput>
+					</div>
 				</aside>
 				<canvas ref={this.canvas} />
 				<div
--- src/colorFunctions.ts
+++ src/colorFunctions.ts
@@ -2,3 +2,70 @@ export function getAccentColor(): string {
 	return window.getComputedStyle(document.body)
 		.getPropertyValue("--accent").trim();
 }
+
+export function getComplimentColor(): string {
+	const accent = getAccentColor().substr(1);
+	const [h, s, l] = RGB2HSL(
+		parseInt(accent.substr(0, 2), 16),
+		parseInt(accent.substr(2, 2), 16),
+		parseInt(accent.substr(4, 2), 16)
+	);
+	const [r, g, b] = HSL2RGB((h + 0.5) % 1.0, s, l);
+	return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+}
+
+/**
+ * Borrowed from https://stackoverflow.com/a/9493060/5069211
+ */
+function RGB2HSL(r: number, g: number, b: number): [number, number, number] {
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	const max = Math.max(r, b, g);
+	const min = Math.min(r, g, b);
+	const l = (max + min) / 2;
+	const [h, s] = (() => {
+		if (max == min) {
+			return [0, 0];
+		}
+		else {
+			const d = max - min;
+			return [
+				(
+					max == r ? (g - b) / d + (g < d ? 6 : 0) :
+						max == g ? (b - r) / d + 2 :
+							(r - g) / d + 4
+				) / 6,
+				l > 0.5 ? d / (2 - max - min) : d / (max + min)
+			];
+		}
+	})();
+	return [h, s, l];
+}
+
+/**
+ * Borrowed from https://stackoverflow.com/a/9493060/5069211
+ */
+function HSL2RGB(h: number, s: number, l: number): [number, number, number] {
+	if (s === 0) {
+		return [l * 255, l * 255, l * 255];
+	}
+	else {
+		const HUE2RGB = (p, q, t) => {
+			if (t < 0) t++;
+			if (t > 1) t--;
+			if (t < 1 / 6) return p + (q - p) * 6 * t;
+			if (t < 1 / 2) return q;
+			if (t < 2 / 3) return p + (q - p) * 6 * (2 / 3 - t);
+			return p;
+		};
+
+		const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		const p = 2 * l - q;
+		return [
+			Math.round(HUE2RGB(p, q, h + 1 / 3) * 255),
+			Math.round(HUE2RGB(p, q, h) * 255),
+			Math.round(HUE2RGB(p, q, h - 1 / 3) * 255)
+		];
+	}
+}
